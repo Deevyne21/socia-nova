749e6dc87a9f6001df27356003bc93caabcf6298
Divine Johnon <johnsondivine15@gmail.com>
2025-11-06 01:04:19 +0100

    Fix tailwind content part for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
@@ -9,24 +9,13 @@ export default {
         accent: "#73cb4e",
         dark: "#1E1E1E",
         light: "#F8F9FA",
-        // authenticity: "#E8F5E9",
-        // creativity: "#E3F2FD",
-        // proven: "#FFF3E0",
-        // scalable: "#F3E5F5",
-        // authenticityiconbg: "#73cb4e",
-        // creativityiconbg: "#26A69A",
-        // proveniconbg: "#651FFF",
-        // scalableiconbg: "#FF4081",
-        // resulticonbg: "#B3E5FC",
         statsrating: "#FFD700",
       },
-
       fontFamily: {
         heading: ["Sarabun", "sans-serif"],
         body: ["Poppins", "sans-serif"],
       },
     },
   },
-
   plugins: [],
 };
